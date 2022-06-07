71d6b3ea02460e30cd8796772f7f0dca54e4135f
Alexandre <alexandre.choura@gmail.com>
2022-06-07 12:31:11 +0200

    Add Closing Reasons, Tags & Categories to basic seeding

--- src/Seeds/Basic.php
+++ src/Seeds/Basic.php
@@ -9,8 +9,16 @@ use PanicHD\PanicHD\Models\Category;
 class Basic extends Seeder
 {
     public $categories = [
-        'Issues'     => '#ffbc1b',
+        'Bugs'      => '#ffbc1b',
+        'Question'  => '#71ED61',
+        'Demande'   => '#5EC6EC',
+        'Autre'     => '#4e00ff',
     ];
+
+    public $tags = ['e-mail', 'matériel', 'internet', 'SAP', 'logiciel', 'fournisseur', 'article', 'autre'];
+    public $a_bg_color = ['#e6b8af', '#f4cccc', '#fce5cd', '#fff2cc', '#d9ead3', '#d0e0e3', '#c9daf8', '#cfe2f3', '#d9d2e9', '#ead1dc'];
+
+    public $closing_reasons = ['erreur utilisateur', 'erreur logicielle', 'erreur matérielle', 'aucune réponse', 'résolue', 'autre'];
 
     /**
      * Run the database seeds.
@@ -25,6 +33,27 @@ class Basic extends Seeder
         foreach ($this->categories as $name => $color) {
             $category = Category::firstOrNew(['name'  => $name]);
             $category->color = $color;
+
+            // Create category's tags
+            foreach ($this->tags as $tag) {
+                $category->tags()->create([
+                    'name' => $tag,
+                    'bg_color'   => $this->a_bg_color[array_rand($this->a_bg_color)],
+                    'text_color' => '#0c343d',
+                ]);
+            }
+
+            // Create category's closing reasons
+            $ordering = 0;
+            foreach ($this->closing_reasons as $reason) {
+                $category->closingReasons()->create([
+                    'text'          => $reason,
+                    'category_id'   => $category->id,
+                    'ordering'      => $ordering,
+                ]);
+                $ordering++;
+            }
+
             $category->save();
         }
 
