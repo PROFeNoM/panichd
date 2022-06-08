e545d010b05c20afa2410f0b975b9f77309c31fa
Alexandre <alexandre.choura@gmail.com>
2022-06-08 11:27:33 +0200

    Fix typo
    panich_admin -> panichd_admin

--- src/Seeds/Basic.php
+++ src/Seeds/Basic.php
@@ -31,10 +31,10 @@ class Basic extends Seeder
     {
         Model::unguard();
 
-        // Retrieve super-admin users and set panich_admin and panichd_agent to 1
+        // Retrieve super-admin users and set panichd_admin and panichd_agent to 1
         $super_admins = User::where('role_slug', 'super-admin')->get();
         foreach ($super_admins as $super_admin) {
-            $super_admin->panich_admin = 1;
+            $super_admin->panichd_admin = 1;
             $super_admin->panichd_agent = 1;
             $super_admin->save();
         }
