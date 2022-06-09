34116303b9c292b4dedf3e87c06afd0a8943bbf7
Alexandre <alexandre.choura@gmail.com>
2022-06-09 15:52:42 +0200

    Hide owner's form

--- src/Views/tickets/createedit/form.blade.php
+++ src/Views/tickets/createedit/form.blade.php
@@ -61,11 +61,7 @@
                 </div>
             </div>
         @else
-            <select id="owner_id" name="owner_id" class="generate_default_select2 form-control" style="display: none; width: 100%">
-                <option style="display: none" value="{{ $u->id }}" selected="selected">{{ $u->name . ($u->email == "" ? ' ' . trans('panichd::lang.ticket-owner-no-email') : ' - ' . $u->email) }}
-
-                </option>
-            </select>
+            {!! CollectiveForm::hidden('owner_id', $u->id) !!}
         @endif
 
         @if ($u->currentLevel() > 1)
