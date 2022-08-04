9b1834748642fcaab47ee70210570fa6c2e6cd39
Guillaume Hanotel <guillaumehanotel@orange.fr>
2022-08-04 16:23:02 +0200

    fix ticket comment date

--- src/Views/tickets/partials/comments/list.blade.php
+++ src/Views/tickets/partials/comments/list.blade.php
@@ -65,8 +65,8 @@
         <h6 class="card-title mb-0 @if($u->id == $ticket->agent_id && $comment->read_by_agent != '1') text-white @endif">
           <span class="float-right">
 			  @php
-			    $rawFrom = \Carbon\Carbon::parse($comment->created_at);
-			    $rawNow = \Carbon\Carbon::now();
+				$rawFrom = \Carbon\Carbon::parse($comment->created_at);
+				$rawNow = \Carbon\Carbon::now();
                 $from = $rawFrom->format(trans('panichd::lang.datetime-format'));
                 $now =  $rawNow->format(trans('panichd::lang.datetime-format'));
                 $diff_in_days = $rawNow->diffInDays($rawFrom);
